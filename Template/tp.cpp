6be652823ba047ba4e2fc4801ef218b01577681a
Dikson <49994083+diksown@users.noreply.github.com>
2020-04-04 12:18:39 -0300

    Added my default template

---
+++ Template/tp.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back 
+#define fastcin ios::sync_with_stdio(false); cin.tie(NULL);
+#define endl '\n'
+
+int main(){
+}
