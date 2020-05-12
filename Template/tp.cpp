24b0dff506abcd00c7c4c54c9425dc4788f1d5e3
Dikson <49994083+diksown@users.noreply.github.com>
2020-05-12 20:50:10 -0300

    some minor fixes

--- Template/tp.cpp
+++ Template/tp.cpp
@@ -1,8 +1,9 @@
+//WA => CARO
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define pb push_back 
-#define fastcin ios::sync_with_stdio(false); cin.tie(NULL);
+#define fastcin ios::sync_with_stdio(0); cin.tie(0);
 #define endl '\n'
 
 int main(){
