9cfff4bbc117c53c1e5397e22085734b1f0174a8
Dikson <49994083+diksown@users.noreply.github.com>
2020-11-30 05:56:57 -0300

    added simple LIS

---
+++ Miscellaneous/lis.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+
+signed main(){
+	int n; cin >> n;
+	vector<int> dp;
+	for (int i=0; i<n; i++){
+		int x; cin >> x;
+		auto it = lower_bound(dp.begin(), dp.end(), x);
+		if (it == dp.end()) dp.pb(x);
+		else *it = x;
+	}
+	cout << dp.size() << endl;
+}
